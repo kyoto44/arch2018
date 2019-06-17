3f42fc0499ec9c3aba87a2d3241a5d0e3754f05a
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-06-17 21:07:47 +0500

    Update arch3.sh

--- arch3.sh
+++ arch3.sh
@@ -13,7 +13,7 @@ sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
 echo 'Установка программ'
-sudo pacman -S firefox ufw qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
+sudo pacman -S chromium qt4 f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
 
 echo 'Установить рекомендумые программы?'
 read -p "1 - Да, 0 - Нет: " prog_set
@@ -54,22 +54,6 @@ elif [[ $xfce_set == 0 ]]; then
   echo 'Установка конфигов XFCE пропущена.'
 fi 
   
-echo 'Скачать и установить конфиг и темы для Openbox?'
-read -p "1 - Да, 0 - Нет: " openbox_set
-if [[ $openbox_set == 1 ]]; then
-  echo 'Качаем и устанавливаем настройки Openbox'
-  #wget git.io/openbox.tar.gz
-  #sudo tar -xzf openbox.tar.gz -C ~/
-  #wget git.io/tint2.tar.gz
-  #sudo tar -xzf tint2.tar.gz -C ~/
-  wget https://github.com/ordanax/arch2018/raw/master/attach/config.tar.gz
-  sudo tar -xzf config.tar.gz -C ~/
-  yay -S obconf obmenu-generator obkey-git lxappearance-obconf tint2 nitrogen thunar mousepad wmctrl compton papirus-icon-theme
-  sudo pacman -S capitaine-cursors
-elif [[ $openbox_set == 0 ]]; then
-  echo 'Установка конфигов Openbox пропущена.'
-fi  
-
 echo 'Убираем меню граб для выбора системы?'
 read -p "1 - Да, 0 - Нет: " grub_set
 if [[ $grub_set == 1 ]]; then
@@ -79,22 +63,6 @@ if [[ $grub_set == 1 ]]; then
 elif [[ $grub_set == 0 ]]; then
   echo 'Пропускаем.'
 fi
-
-echo 'Установить conky?'
-read -p "1 - Да, 0 - Нет: " conky_set
-if [[ $conky_set == 1 ]]; then
-  sudo pacman -S conky conky-manager --noconfirm
-  wget git.io/conky.tar.gz
-  tar -xzf conky.tar.gz -C $HOME/
-elif [[ $conky_set == 0 ]]; then
-  echo 'Установка conky пропущена.'
-fi
-
-echo 'Включаем сетевой экран'
-sudo ufw enable
-
-echo 'Добавляем в автозагрузку:'
-sudo systemctl enable ufw
 
 sudo rm -rf ~/Downloads
 sudo rm -rf ~/arch3.sh
