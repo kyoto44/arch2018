4c2ca90f2cecdd0cc2bd2d77c650a7ee2c2c90c6
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-06-18 19:48:17 +0500

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -17,36 +17,6 @@ echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
 echo '2.4 создание разделов'
-(
-  echo o;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +100M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +20G;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +1024M;
-
-  echo n;
-  echo p;
-  echo;
-  echo;
-  echo a;
-  echo 1;
-
-  echo w;
-) | fdisk /dev/sda
 
 echo 'Ваша разметка диска'
 fdisk -l
